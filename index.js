15c5ff62715de43f5638401bc237640ee3860869
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-03-23 20:21:26 +0300

    Move segments path to .env and ensure that directory exists

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const EventEmitter = require('events');
 const os = require('os');
 const ip = require('ip');
 const _ = require('lodash');
+const mkdirp = require('mkdirp');
 
 if (!process.env.MASTERSERVER || !process.env.NAME || !process.env.MASTERSERVERIP) {
     console.error('No master-server or name specified, or .env file is missing');
     process.exit(1);
 }
+
+if (!process.env.SEGMENTSPATH) {
+    console.error('Path for segments is not specified. Exiting');
+    process.exit(1);
+}
+
+console.log('Trying to create directory ' + process.env.SEGMENTSPATH);
+mkdirp.sync(process.env.PATH); //create directory
 
 let myip = null;
 let myInterfaces = os.networkInterfaces();
@@ -36,7 +45,7 @@ let crf = 30; // Bitrate option in x264 codec
 const heartbeatInterval = 300000;
 
 function sendHearbeat() {
-    console.log('Sending heartbeat to ',  process.env.MASTERSERVER + '/heartbeat');
+    console.log('Sending heartbeat to ' + process.env.MASTERSERVER + '/heartbeat');
     let heartbeatData = {
         baseUrl: 'http://' + myip,
         name: process.env.NAME,
@@ -60,7 +69,7 @@ function generateFfmpegOptions(crf) {
     return ['-loglevel', 'panic', '-i', '/dev/video0', '-f', 'v4l2', '-c:v', 'libx264', '-r', '8', '-g', '8', '-crf', crf.toString(),
         '-pix_fmt', 'yuv420p', '-tune', 'zerolatency', '-profile:v', 'baseline', '-preset:v', 'ultrafast', '-map', '0',
         '-f', 'hls', '-use_localtime', '1', '-hls_time', '2', '-hls_list_size', '3', '-hls_flags', 'delete_segments',
-        '-hls_segment_filename', '/tmp/cam/segment-%Y-%m-%d_%H-%M-%S.ts', '/tmp/cam/live.m3u8'];
+        '-hls_segment_filename', process.env.SEGMENTSPATH + '/segment-%Y-%m-%d_%H-%M-%S.ts', process.env.SEGMENTSPATH + '/live.m3u8'];
 }
 
 class Ffmpeg extends EventEmitter {
