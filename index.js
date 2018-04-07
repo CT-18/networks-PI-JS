6a400e6f11b86c4020db0be88bc25c31fa3eb9b8
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-04-07 14:08:23 +0300

    Heartbeat success logging

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const spawn = require('child_process').spawn;
+const fs = require('fs');
 const express = require('express');
 const request = require('request');
 const ffmpeg = require('fluent-ffmpeg');
 const cors = require('cors');
 const mkdirp = require('mkdirp');
 
+const dir = '/tmp/camera';
+
 if (!process.env.MASTERSERVER || !process.env.NAME) {
     console.error('No master-server or name specified, or .env file is missing');
     process.exit(1);
 }
 
-mkdirp.sync('/tmp/camera');
+mkdirp.sync(dir);
 
 let masterServer = process.env.MASTERSERVER;
 if (!masterServer.startsWith('http://')) {
@@ -19,10 +22,13 @@ if (!masterServer.startsWith('http://')) {
 }
 
 let port = process.env.PORT || 8080;
-let bitrate = 3 * 1000 * 1000;
+let bitrateMultiplier = 5;
 const heartbeatInterval = 300000;
 let ffmpegInputOptions = ['-loglevel', 'panic'];
-let ffmpegOutputOptions = ['-vcodec copy', '-use_localtime 1', '-hls_time 1', '-hls_list_size 4', '-hls_flags delete_segments+split_by_time', '-hls_segment_filename /tmp/camera/segment-%Y-%m-%d_%H-%M-%S.ts'];
+let ffmpegOutputOptions = ['-vcodec copy', '-use_localtime 1', '-hls_time 2', '-hls_list_size 2', '-hls_flags delete_segments+split_by_time', '-hls_segment_filename /tmp/camera/segment-%Y-%m-%d_%H-%M-%S.ts'];
+
+let cameraStream = null;
+let conversion = null;
 
 function sendHearbeat() {
     console.log('Sending heartbeat to ' + masterServer + '/heartbeat');
@@ -46,38 +52,33 @@ function sendHearbeat() {
     );
 }
 
-function generateRaspividOptions(bitrate) {
-    return ['-o', '-', '-t', '0', '-n', '-h', '1080', '-w', '1920', '-ih', '-pf', 'baseline', '-fps', '30', '-g', '30', '-b', bitrate.toString(), '-fl'];
+function launchStream() {
+    cameraStream = spawn('raspivid', ['-o', '-', '-t', '0', '-n', '-h', '1080', '-w', '1920', '-ih', '-pf', 'baseline', '-fps', '30', '-g', '30', '-b', (bitrateMultiplier * 1000 * 1000).toString(), '-fl']);
+    console.log(`Spawned raspivid with ${(bitrateMultiplier * 1000 * 1000)} bitrate`);
+    conversion = new ffmpeg(cameraStream.stdout).noAudio().format('hls').inputOptions(ffmpegInputOptions).outputOptions(ffmpegOutputOptions).output(`/tmp/camera/live.m3u8`);
+    cameraStream.stderr.on('data', function (data) {
+        console.log('Error while connecting to camera: ' + data.toString());
+        process.exit(1);
+    });
+
+    conversion.on('start', function(commandLine) {
+        console.log('Spawned Ffmpeg with command: ' + commandLine);
+    });
+
+    conversion.on('stderr', function(stderrLine) {
+        console.log('Cannot process video: ' + stderrLine);
+        process.exit(1);
+    });
+
+    conversion.run();
 }
-let cameraStream = spawn('raspivid', generateRaspividOptions(bitrate));
-let conversion = new ffmpeg(cameraStream.stdout).noAudio().format('hls').inputOptions(ffmpegInputOptions).outputOptions(ffmpegOutputOptions).output(`/tmp/camera/live.m3u8`);
-cameraStream.stderr.on('data', function (data) {
-    console.log('Error while connecting to camera: ' + data.toString());
-    process.exit(1);
-});
-conversion.on('error', function(err, stdout, stderr) {
-    console.log('Cannot process video: ' + err.message);
-    process.exit(1);
-});
 
-conversion.on('start', function(commandLine) {
-    console.log('Spawned Ffmpeg with command: ' + commandLine);
-});
-
-conversion.on('stderr', function(stderrLine) {
-    console.log('Stderr output: ' + stderrLine);
-});
-
-conversion.run();
 let app = express();
-
 app.use(cors());
-
-
-app.use(`/`, express.static('/tmp/camera'));
-
+app.use(`/`, express.static(dir));
 app.listen(port);
 console.log(`STARTING CAMERA STREAM SERVER AT PORT ${port}`);
 
+launchStream();
 sendHearbeat();
 setInterval(sendHearbeat, heartbeatInterval);
